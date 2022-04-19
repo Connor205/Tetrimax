25c5f62dcf986a35773d456505a44cd8cfbd8593
Connor Nelson <Conn205@gmail.com>
2022-04-19 14:35:20 -0400

    Adding in csv to neatFactory

--- neatFactory.py
+++ neatFactory.py
@@ -1,11 +1,15 @@
 from __future__ import print_function
+import csv
+import os
+import neat
+import math
+import time
+
 from tetrisAgent import NeatAgent
 from tetrisSimulation import TetrisSimulation
 from hueristics import featureVector
 
-import os
-import neat
-import math
+NUM_GAMES = 10
 
 
 def selu_activation(z):
@@ -17,13 +21,13 @@ def selu_activation(z):
 def eval_single_genome(genome, config):
     total = 0
     net = neat.nn.FeedForwardNetwork.create(genome, config)
-    for _ in range(10):
+    for _ in range(NUM_GAMES):
         agent = NeatAgent(featureVector, net)
         sim = TetrisSimulation(agent)
         board, score, survived, boards, moves, pieces, linesCleared = sim.playGame(
             scoringType='tetris')
         total += score
-    total /= 10
+    total /= NUM_GAMES
     # print("Finished Evaluating Genome")
     return total
 
@@ -33,7 +37,7 @@ def eval_genomes(genomes, config):
     evaluator.evaluate(genomes, config)
 
 
-def run(config_file, checkpoint_file: str = None):
+def run(config_file, checkpoint_file: str = None, csv_file=None):
     # Load configuration.
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -51,12 +55,24 @@ def run(config_file, checkpoint_file: str = None):
     # Add a stdout reporter to show progress in the terminal.
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
+    stats
     p.add_reporter(stats)
-    p.add_reporter(
-        neat.Checkpointer(5, filename_prefix='neat-checkpoints/selu-'))
+    # p.add_reporter(
+    #     neat.Checkpointer(5, filename_prefix='neat-checkpoints/selu-'))
 
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 100)
+    winner = p.run(eval_genomes, 5)
+
+    if csv_file is not None:
+        with open(csv_file, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(
+                ["Generation", "Max Score", "Total Score", "Average Score"])
+            means = stats.get_fitness_mean()
+            maxs = stats.get_fitness_stat(max)
+            totals = stats.get_fitness_stat(sum)
+            for i in range(len(means)):
+                writer.writerow([i, maxs[i], totals[i], means[i]])
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
@@ -65,10 +81,10 @@ def run(config_file, checkpoint_file: str = None):
     print("Lets see how it does in 20 games")
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
     for i in range(20):
-        agent = NeatAgent(winner_net)
-        sim = TetrisSimulation(featureVector, agent)
+        agent = NeatAgent(featureVector, winner_net)
+        sim = TetrisSimulation(agent)
         board, score, survived, boards, moves, pieces, linesCleared = sim.playGame(
-            scoringByLines=False)
+            scoringType='tetris')
         print("Game {}: Score: {}".format(i, score))
         print("Game {}: Lines Cleared: {}".format(i, linesCleared))
         print("Game {}: Survived: {}".format(i, survived))
@@ -80,4 +96,4 @@ if __name__ == '__main__':
     # current working directory.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'tetrisagentconfigselu')
-    run(config_path, 'neat-checkpoints/selu-4')
+    run(config_path, csv_file="test-csv.csv")
